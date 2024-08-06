484572a996f302d40c71bfa3d64599158efeb46a
Hamza <yousfihamza22@gmail.com>
2024-08-06 22:31:43 +0200

    chose initial cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Hamza Yousfi",
-  description: "Hamza's Portfolio",
+  description: "Hamza's Modern Portfolio !",
 };
 
 export default function RootLayout({
